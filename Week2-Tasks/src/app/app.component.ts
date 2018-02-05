c99091d0e290b15d5a570d5969b08da0b136b3ba
killian <killiank360@gmail.com>
2018-02-05 18:11:48 +0000

    Finished styled reddit app.

--- Week2-Tasks/src/app/app.component.ts
+++ Week2-Tasks/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io'),
+      new Article('Fullstack', 'http://fullstack.io'),
+      new Article('Angular Homepage', 'http://angular.io')
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article...Title= ${title.value} Link= ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    title.value = null;
+    link.value = null;
+    return false;
+  }
 }
